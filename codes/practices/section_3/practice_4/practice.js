73c37440ff0f9bb3ccdd6489a49573c127e8fb87
chenyue <15532196053@163.com>
2014-11-18 18:18:31 +0800

    添加练习题，求某些学生的平均值

--- codes/practices/section_3/practice_4/practice.js
+++ codes/practices/section_3/practice_4/practice.js
@@ -11,13 +11,7 @@ var stu_scores = {'杨璐':131,
 var stu_names = ['刘帅','康惠雯','沙龙逸','王雪','杨璐','李丽'];
 var stu_names_scores = {};
 //获取数组stu_names中学生的成绩
-for(var i = 0; i < stu_names.length; i ++)
-{
-    if(stu_scores[stu_names[i]] !== undefined)
-    {
-        stu_names_scores[stu_names[i]] = stu_scores[stu_names[i]];
-    }
-}
+
 //打印哈希数组stu_names_scores中学生成绩
 console.log('刘帅成绩：'+stu_names_scores['刘帅']);
 console.log('康惠雯成绩：'+stu_names_scores['康惠雯']);
