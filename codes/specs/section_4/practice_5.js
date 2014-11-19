78a04512f4315287d009be4d349198f42007a25d
chenyue <15532196053@163.com>
2014-11-19 16:11:57 +0800

    完成字符串的第六个练习

--- codes/specs/section_4/practice_5.js
+++ codes/specs/section_4/practice_5.js
@@ -1,6 +1,6 @@
 describe('practice-4-5', function () {
 
-    it("print string 李鉴学时第几个学生，他的语文成绩为：126", function () {
+    it("print string is 李鉴学时第几个学生，他的语文成绩为：126", function () {
         console.dir(logs);
         expect(logs[0]).toBe("李鉴学是第4个学生，他的语文成绩为：126");
         expect(logs[1]).toBe("李鉴学是第4个学生，他的数学成绩为：135");
---
+++ section_4/practice_6_answer.md
@@ -0,0 +1,7 @@
+- 在第14行添加如下代码：
+
+        for(var i = 0; i < stu_names.length; i ++)
+        {
+            var str_stu_score = stu_names[i]+'的成绩为：'+'语文：'+all_scores[stu_names[i]][0]+';'+'数学：'+all_scores[stu_names[i]][1]+';'+'英语：'+all_scores[stu_names[i]][2]+';';
+            console.log(str_stu_score);
+        }
