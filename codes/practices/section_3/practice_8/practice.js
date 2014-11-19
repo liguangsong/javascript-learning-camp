76763194ba2b5d8623038fc35a4e255471bb5afd
chenyue <15532196053@163.com>
2014-11-19 09:31:22 +0800

    删除第三部分第八个练习的答案

--- codes/practices/section_3/practice_8/practice.js
+++ codes/practices/section_3/practice_8/practice.js
@@ -10,13 +10,4 @@ var all_scores = {'杨璐':[131,142,144],
     '林世博':[116,142,129]};
 //存储学生的姓名
 var stu_names = ['杨璐','王雪','韩林霖','沙龙逸','李鉴学'];
-//求stu_names中学生的平均成绩
-for(var i = 0; i < stu_names.length; i ++)
-{
-    var sum_score = 0;
-    for(var j = 0; j < all_scores[stu_names[i]].length; j++)
-    {
-        sum_score += all_scores[stu_names[i]][j];
-    }
-    console.log(sum_score/all_scores[stu_names[i]].length);
-}
+//求stu_names中学生的平均成绩并打印
