35527a2c8de6a6712df0a014af60c416ba148e16
chenyue <15532196053@163.com>
2014-11-21 13:41:43 +0800

    修改函数练习一的测试

--- codes/specs/section_6/practice_1.js
+++ codes/specs/section_6/practice_1.js
@@ -2,6 +2,7 @@ describe('practice-6-1', function () {
 
     it("use get_product_of_two_num(3,5) is 15 ", function () {
         var product = get_product_of_two_num(3,5);
+        console.log('3*5='+product);
         expect(product).toBe(15);
     });
 
